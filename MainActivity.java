411d3a277c9e3ac03b4b30e3a7746be3341a07ec
Sarah Moreira Messias <79113261+sarahmmessias@users.noreply.github.com>
2022-08-29 14:29:36 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.pamiia2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myButtonListenerMethod();
+    }
+
+
+    public void myButtonListenerMethod() {
+        final Button button = (Button) findViewById(R.id.mybutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConstraintLayout bgElement = (ConstraintLayout) findViewById(R.id.activity_main);
+                int color = ((ColorDrawable) bgElement.getBackground()).getColor();
+                if (color == Color.BLUE) {
+                    bgElement.setBackgroundColor(Color.RED);
+                } else {
+                    bgElement.setBackgroundColor(Color.BLUE);
+                }
+            }
+        });
+    }
+
+
+}
