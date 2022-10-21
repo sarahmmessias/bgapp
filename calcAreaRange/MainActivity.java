eca0edfc1a83e789d390dc0ed959dab34a6ae69e
Sarah Moreira Messias <79113261+sarahmmessias@users.noreply.github.com>
2022-10-21 10:51:59 -0300

    Update MainActivity.java

--- calcAreaRange/MainActivity.java
+++ calcAreaRange/MainActivity.java
@@ -1,9 +1,11 @@
-package br.com.br.calcarearange;
+package br.com.bq.calcarearange;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -13,35 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        calc();
+        rangeAlturaSelectListener();
+        rangeLarguraSelectListener();
     }
 
-    public void calc()
-    {
-        /*final Button button = (Button) findViewById(R.id.b_calc);
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                double height = Double.parseDouble(((EditText) findViewById(R.id.e_height)).getText().toString());
-                double width = Double.parseDouble(((EditText) findViewById(R.id.e_width)).getText().toString());
-                TextView area = (TextView) findViewById(R.id.area_text);
-                DecimalFormat df = new DecimalFormat("#.#");
-                double area_total = height * width;
-                String s = "Área = " + Double.parseDouble(df.format(height * width)) + " u²";
-                area.setText(s);
+    public void calculaArea() {
+        final EditText outputArea = findViewById(R.id.outputArea);
+        final TextView textSeekBarLargura = findViewById(R.id.textSeekBarLargura);
+        final TextView textSeekBarAltura = findViewById(R.id.textSeekBarAltura);
+        double alturaDecimal = Double.parseDouble(textSeekBarAltura.getText().toString());
+        double larguraDecimal = Double.parseDouble(textSeekBarLargura.getText().toString());
+        double areaDecimal = alturaDecimal * larguraDecimal;
+        outputArea.setText(Double.toString(areaDecimal));
 
-            }
-        });*/
-        final SeekBar s_width = (SeekBar) findViewById(R.id.w_slide);
-        final SeekBar s_height = (SeekBar) findViewById(R.id.h_slide);
-        final TextView area_view = (TextView) findViewById(R.id.area_text);
-        SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener(){
+    }
+
+    public void rangeLarguraSelectListener() {
+        SeekBar seekBarTest = findViewById(R.id.seekBar5);
+        final TextView textSeekBarLargura = findViewById(R.id.textSeekBarLargura);
+        seekBarTest.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                int width = (int) s_width.getProgress();
-                int height = (int) s_height.getProgress();
-                area_view.setText("Área = " + width*height + "u²");
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textSeekBarLargura.setText(String.valueOf(i));
+                calculaArea();
             }
 
             @Override
@@ -53,10 +49,28 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
-        };
-        s_height.setOnSeekBarChangeListener(listener);
-        s_width.setOnSeekBarChangeListener(listener);
+        });
     }
 
+    public void rangeAlturaSelectListener() {
+        SeekBar seekBarTest = findViewById(R.id.seekBar3);
+        final TextView textSeekBarAltura = findViewById(R.id.textSeekBarAltura);
+        seekBarTest.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textSeekBarAltura.setText(String.valueOf(i));
+                calculaArea();
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
 }
